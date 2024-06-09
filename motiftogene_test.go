218379f7dee75e086bbdab95719e3219df2f971b
Antony Holmes <antony@antonybholmes.com>
2024-06-09 12:09:03 -0400

    fix: Added tests

---
+++ motiftogene_test.go
@@ -0,0 +1,19 @@
+package motiftogene
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGenes(t *testing.T) {
+
+	db := NewMotifToGeneDB("data/modules/motiftogene/motiftogene.json")
+
+	gene, err := db.Convert("ADNP_IRX_SIX_ZHX.p2")
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	fmt.Printf("%v %v", gene.Databases, gene.Genes)
+}
